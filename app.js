7ed2d39ca1ca7fdd2d24f495849c3f9cd6e52682
Brian Triplett <btriplett1990@yahoo.com>
2024-06-09 22:37:26 -0400

    change to use keys instead of buttons

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const addNewTask = document.querySelector('#addNewTask');
-const deleteCompletedTasks = document.querySelector('#deleteCompletedTasks');
 const list = document.querySelector('#list');
+const modalBackground = document.querySelector(".modal-background");
+const popupContainer = document.querySelector(".popup-container");
+
+let confirmIsOpen = false;
 
 let previousTasks = JSON.parse(localStorage.getItem("tasks"));
 if(previousTasks) {
@@ -26,16 +28,6 @@ function addTaskToStorage(task) {
         localStorage.setItem("tasks", JSON.stringify([task]));
     }
 }
-
-addNewTask.addEventListener('click', () => {
-    let newTask = prompt('What would you like to add?');
-    if(newTask !== null && newTask !== ''){
-        let taskId = crypto.randomUUID();
-        addTaskToList(newTask, `task${taskId}`);
-        let obj = new task(newTask, `task${taskId}`);
-        addTaskToStorage(obj, taskId);
-    }
-})
 
 function addTaskToList(newTask, taskId, isCompleted = false) {
     let newDiv = document.createElement('div');
@@ -71,16 +63,49 @@ function createEventListener(newCheckbox) {
     })
 }
 
-
-deleteCompletedTasks.addEventListener('click', () => {
-    allCheckbox = document.querySelectorAll('input');
-    for(checkbox of allCheckbox){
-        if(checkbox.checked) {
-            checkbox.parentElement.remove();
+document.addEventListener("keydown", (e) => {
+    if (!confirmIsOpen && (e.key === "a" || e.key === "A")) {
+        let newTask = prompt('What would you like to add?');
+        if(newTask !== null && newTask !== ''){
+            let taskId = crypto.randomUUID();
+            addTaskToList(newTask, `task${taskId}`);
+            let obj = new task(newTask, `task${taskId}`);
+            addTaskToStorage(obj, taskId);
         }
     }
+})
 
-    let tasks = JSON.parse(localStorage.getItem("tasks"));
-    tasks = tasks.filter(task => task.isCompleted === false);
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+document.addEventListener("keydown", (e) => {
+    if (e.key === "d" || e.key === "D") {
+        modalBackground.classList.add('visible');
+        confirmIsOpen = true;
+    }
+})
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+        modalBackground.classList.remove('visible');
+        confirmIsOpen = false;
+    }
+})
+
+document.addEventListener("keydown", (e) => {
+    if (confirmIsOpen && (e.key === "y" || e.key === "Y")) {
+        allCheckbox = document.querySelectorAll('input');
+        for(checkbox of allCheckbox){
+            if(checkbox.checked) {
+                checkbox.parentElement.remove();
+            }
+        }
+
+        let tasks = JSON.parse(localStorage.getItem("tasks"));
+        tasks = tasks.filter(task => task.isCompleted === false);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+
+        modalBackground.classList.remove("visible");
+        confirmIsOpen = false;
+    } else if (confirmIsOpen && (e.key === "n" || e.key === "N")) {
+        modalBackground.classList.remove("visible");
+        confirmIsOpen = false;
+    }
 })
